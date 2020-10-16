e943737661708ca4ad72ee719f1d74ebb01792d9
kevin trentini <trentinikevin6@gmail.com>
2020-10-16 22:08:38 +0200

    Add project files.

---
+++ creditoResiduoBancomat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace creditoResiduoBancomat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
